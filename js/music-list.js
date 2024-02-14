5c4643d3d3e9fa413c6140e2796807388f5480fc
leomasa <kevincutipa228@gmail.com>
2024-02-13 23:40:31 -0300

    para mi amor

---
+++ js/music-list.js
@@ -0,0 +1,135 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Acurrucar",
+    artist: "Ed Maverick",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Fuentes de Ortiz",
+    artist: "Ed Maverick",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "San Lucas",
+    artist: "Kevin Kaarl",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Abrazado a Ti",
+    artist: "Kevin Kaarl",
+    img: "music-10",
+    src: "music-11"
+  },
+  {
+    name: "Ropa De Bazar (con BRATTY)",
+    artist: "Ed Maverick",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Ayer y hoy",
+    artist: "Julio Jaramillo",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Algo contigo",
+    artist: "Rita Payés y Elisabeth Roma",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Where You Are",
+    artist: "Christopher Jackson, Rachel House, Nicole Scherzinger y Auli'i Cravalho",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Un Poco Loco",
+    artist: "Luis Ángel Gómez Jaramillo y Gael García Bernal",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Un Amigo Fiel",
+    artist: "Arturo Mercado Jr",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Con Toda Libertad",
+    artist: "Yuridia",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "De nada",
+    artist: "Beto Castillo",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Hombres de Acción Serán Hoy",
+    artist: "Cristian",
+    img: "music-20",
+    src: "music-20"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
